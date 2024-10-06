877f12a583d1f102ab4afe369e1ae41c789cf15b
Anna Mamedov <mamedovaanna.i@gmail.com>
2024-10-06 16:17:11 +0300

    сдача проектной работы

--- scripts/index.js
+++ scripts/index.js
@@ -6,27 +6,28 @@ const cardAddButton = document.querySelector('.profile__add-button');
 const places_list = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
-const cardAdd = function (name, link, alt) {
+const createCard = function (name, link, deleteCard) {
     const cardElement = cardTemplate.content.querySelector('.card').cloneNode(true);
-    
+    const cardImage = cardElement.querySelector('.card__image');
+
     cardElement.querySelector('.card__title').textContent = name;
-    cardElement.querySelector('.card__image').src = link;
-    cardElement.querySelector('.card__image').alt = name;
+    cardImage.src = link;
+    cardImage.alt = name;
 
     const cardDeleteButton = cardElement.querySelector('.card__delete-button');
-    cardDeleteButton.addEventListener('click', deleteButton);
+    cardDeleteButton.addEventListener('click', deleteCard);
 
     return cardElement;
 }
 
 // @todo: Функция удаления карточки
-const deleteButton = function(event) {
+const deleteCard = function(event) {
     const cardItem = event.target.closest('.card');
     cardItem.remove();
 };
 
 // @todo: Вывести карточки на страницу
 initialCards.forEach((element) => {
-    const itemElement = cardAdd(element.name, element.link, element.alt);
+    const itemElement = createCard(element.name, element.link, deleteCard);
     places_list.append(itemElement);
 });
